eddc65c4fb657d1adde501a1d3eb33b17217327a
David Bosschaert <david.bosschaert@gmail.com>
2016-12-08 03:35:04 -0800

    Comment on the build script

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
+# Go compilation flags to make the executable small (-s -w) and to make it 
+# work with alpine linux
 CGO_ENABLED=0 go build -ldflags="-s -w" -a -installsuffix cgo -o bin/capserv main.go
 
